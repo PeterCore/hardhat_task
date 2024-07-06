6315b172507463c88190c726f6756c48e24154aa
zhangchun <zhangchun@oamicnet.com>
2024-07-06 15:26:50 +0800

    deploy config

--- test/ArrayShift.js
+++ test/ArrayShift.js
@@ -1,12 +1,14 @@
-const {
-    time,
-    loadFixture,
-} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 const { describe } = require("mocha");
-
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 describe('ArrayShift', function () {
-
+    // async function deployContractArrayShift() {
+    //     const ArrayShift = await ethers.getContractFactory("ArrayShift");
+    //     const arrayShift = await ArrayShift.deploy();
+    //     return { arrayShift };
+    // }
     let arrayShift;
     beforeEach(async function () {
         const ArrayShift = await ethers.getContractFactory("ArrayShift");
@@ -14,16 +16,18 @@ describe('ArrayShift', function () {
     });
 
     it("array remove ", async function () {
+        // const arrayShift = await loadFixture(deployContractArrayShift);
         await arrayShift.removeElement(1);
         const result = await arrayShift.getArray();
-        expect(result).to.deep.equal([1, 3, 4, 5, 5]);
+        expect(result).to.deep.equal([1, 3, 4, 5, 6]);
     });
 
 
     it("array gas lower remove ", async function () {
+        // const arrayShift = await loadFixture(deployContractArrayShift);
         await arrayShift.lowerGasRemove(1);
         const result = await arrayShift.getArray();
-        expect(result).to.deep.equal([1, 5, 3, 4, 5]);
+        expect(result).to.deep.equal([1, 6, 3, 4, 5]);
     });
 
 })
